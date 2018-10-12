58f253564d42e90a72838151f17556a945a27145
Rich Folsom <rich.folsom@gmail.com>
2018-10-11 23:29:43 -0500

    added gitignore

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ import numpy as np
 print('numpy:{}'.format(np.__version__))
 
 
-TRAINING_TIME_LIMIT = 120 * 60
+
 # 8192 - large enough for demonstration, larger values make network training slower
 MAX_VOCAB_SIZE = 2**13
 # seq2seq generally relies on fixed length message vectors - longer messages provide more info
@@ -47,55 +47,8 @@ SUB_BATCH_SIZE = 1000
 
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import casual_tokenize
-
-
-tweets = pd.read_csv('/floyd/input/data/twcs.csv')
-#tweets = pd.read_csv('../data/twcs.csv')
-
-first_inbound = tweets[pd.isnull(tweets.in_response_to_tweet_id) & tweets.inbound]
-
-inbounds_and_outbounds = pd.merge(first_inbound, tweets, left_on='tweet_id', 
-                                  right_on='in_response_to_tweet_id').sample(frac=1)
-
-# Filter to only outbound replies (from companies)
-inbounds_and_outbounds = inbounds_and_outbounds[inbounds_and_outbounds.inbound_y ^ True]
-
-print('Data shape: {}'.format(inbounds_and_outbounds.shape))
-
-
-# Replace anonymized screen names with common token @__sn__
-def sn_replace(match):
-    _sn = match.group(2).lower()
-    if not _sn.isnumeric():
-        # This is a company screen name
-        return match.group(1) + match.group(2)
-    return '@__sn__'
-
-
-sn_re = re.compile('(\W@|^@)([a-zA-Z0-9_]+)')
-print("Replacing anonymized screen names in X...")
-x_text = inbounds_and_outbounds.text_x.apply(lambda txt: sn_re.sub(sn_replace, txt))
-print("Replacing anonymized screen names in Y...")
-y_text = inbounds_and_outbounds.text_y.apply(lambda txt: sn_re.sub(sn_replace, txt))
-
-
-count_vec = CountVectorizer(tokenizer=casual_tokenize, max_features=MAX_VOCAB_SIZE - 3)
-
 from sklearn.externals import joblib
-joblib.dump(count_vec, 'count_vec.joblib') 
-
-
-print("Fitting CountVectorizer on X and Y text data...")
-count_vec.fit(x_text + y_text)
-analyzer = count_vec.build_analyzer()
-vocab = {k: v + 3 for k, v in count_vec.vocabulary_.items()}
-vocab['__unk__'] = UNK
-vocab['__pad__'] = PAD
-vocab['__start__'] = START
-# Used to turn seq2seq predictions into human readable strings
-reverse_vocab = {v: k for k, v in vocab.items()}
-print("Learned vocab of {} items.".format(len(vocab)))
-joblib.dump(vocab, 'vocab.joblib')
+    
 
 
 def to_word_idx(sentence):
@@ -105,26 +58,93 @@ def to_word_idx(sentence):
 def from_word_idx(word_idxs):
     return ' '.join(reverse_vocab[idx] for idx in word_idxs if idx != PAD).strip()
 
-print("Calculating word indexes for X...")
-x = pd.np.vstack(x_text.apply(to_word_idx).values)
-print("Calculating word indexes for Y...")
-y = pd.np.vstack(y_text.apply(to_word_idx).values)
 
 
 
-all_idx = list(range(len(x)))
-train_idx = set(random.sample(all_idx, int(0.8 * len(all_idx))))
-test_idx = {idx for idx in all_idx if idx not in train_idx}
 
-train_x = x[list(train_idx)]
-test_x = x[list(test_idx)]
-train_y = y[list(train_idx)]
-test_y = y[list(test_idx)]
+#REPLACE WITH SQLITE CODE
+#------------------------------------------------------------------------------------------------------------
 
-assert train_x.shape == train_y.shape
-assert test_x.shape == test_y.shape
+def read_csv_data(filename):
+    tweets = pd.read_csv(filename)
+    #tweets = pd.read_csv('/floyd/input/data/twcs.csv')
 
-print('Training data of shape {} and test data of shape {}.'.format(train_x.shape,test_x.shape))
+    first_inbound = tweets[pd.isnull(tweets.in_response_to_tweet_id) & tweets.inbound]
+
+    inbounds_and_outbounds = pd.merge(first_inbound, tweets, left_on='tweet_id', 
+                                    right_on='in_response_to_tweet_id').sample(frac=1)
+
+    # Filter to only outbound replies (from companies)
+    inbounds_and_outbounds = inbounds_and_outbounds[inbounds_and_outbounds.inbound_y ^ True]
+
+    texts = inbounds_and_outbounds.text_x
+    responses = inbounds_and_outbounds.text_y
+
+    print('Data shape: {}'.format(inbounds_and_outbounds.shape))
+    print('t shape: {}'.format(texts.shape))
+    print('r shape: {}'.format(responses.shape))
+    return texts, responses
+
+#--------------------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------------------
+#FUNCTIONALIZE THIS INTO BUILD_VOCAB() FUNCTION
+
+def build_vocab(texts, responses):
+    count_vec = CountVectorizer(tokenizer=casual_tokenize, max_features=MAX_VOCAB_SIZE - 3)
+
+    joblib.dump(count_vec, 'count_vec.joblib') 
+
+
+    print("Fitting CountVectorizer on X and Y text data...")
+    count_vec.fit(texts, responses)
+    analyzer = count_vec.build_analyzer()
+    vocab = {k: v + 3 for k, v in count_vec.vocabulary_.items()}
+    vocab['__unk__'] = UNK
+    vocab['__pad__'] = PAD
+    vocab['__start__'] = START
+    # Used to turn seq2seq predictions into human readable strings
+    reverse_vocab = {v: k for k, v in vocab.items()}
+    print("Learned vocab of {} items.".format(len(vocab)))
+    joblib.dump(vocab, 'vocab.joblib')
+    return vocab, reverse_vocab, analyzer
+#---------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+#BUILD TRAIN & TEST VECTORS
+
+def build_vectors(texts, responses, analyzer):
+
+    print("Calculating word indexes for X...")
+    x = pd.np.vstack(texts.apply(to_word_idx).values)
+    print("Calculating word indexes for Y...")
+    y = pd.np.vstack(responses.apply(to_word_idx).values)
+
+
+
+    all_idx = list(range(len(x)))
+    train_idx = set(random.sample(all_idx, int(0.8 * len(all_idx))))
+    test_idx = {idx for idx in all_idx if idx not in train_idx}
+
+    train_x = x[list(train_idx)]
+    test_x = x[list(test_idx)]
+    train_y = y[list(train_idx)]
+    test_y = y[list(test_idx)]
+
+    assert train_x.shape == train_y.shape
+    assert test_x.shape == test_y.shape
+
+    print('Training data of shape {} and test data of shape {}.'.format(train_x.shape,test_x.shape))
+    return train_x, test_x, train_y, test_y
+#---------------------------------------------------------------------------------------------------------------
+
+
 
 
 # keras imports, because there are like... A million of them.
@@ -199,28 +219,24 @@ def create_model():
     
     return Model(inputs=[encoder_input, last_word_input], outputs=[next_word])
 
-s2s_model = create_model()
-optimizer = Adam(lr=LEARNING_RATE, clipvalue=5.0)
-s2s_model.compile(optimizer='adam', loss='categorical_crossentropy')
-
 
 def add_start_token(y_array):
-    """ Adds the start token to vectors.  Used for training data. """
+    #Adds the start token to vectors.  Used for training data. 
     return np.hstack([
         START * np.ones((len(y_array), 1)),
         y_array[:, :-1],
     ])
 
 def binarize_labels(labels):
-    """ Helper function that turns integer word indexes into sparse binary matrices for 
-        the expected model output.
-    """
+    # Helper function that turns integer word indexes into sparse binary matrices for 
+    #    the expected model output.
+    
     return np.array([np_utils.to_categorical(row, num_classes=MAX_VOCAB_SIZE)
                      for row in labels])
 
 
 def respond_to(model, text):
-    """ Helper function that takes a text input and provides a text output. """
+    # Helper function that takes a text input and provides a text output. 
     input_y = add_start_token(PAD * np.ones((1, MAX_MESSAGE_LEN)))
     idxs = np.array(to_word_idx(text)).reshape((1, MAX_MESSAGE_LEN))
     for position in range(MAX_MESSAGE_LEN - 1):
@@ -229,9 +245,9 @@ def respond_to(model, text):
     return from_word_idx(model.predict([idxs, input_y]).argmax(axis=2)[0])
 
 def train_mini_epoch(model, start_idx, end_idx):
-    """ Batching seems necessary in Kaggle Jupyter Notebook environments, since
-        `model.fit` seems to freeze on larger batches (somewhere 1k-10k).
-    """
+    # Batching seems necessary in Kaggle Jupyter Notebook environments, since
+    #    `model.fit` seems to freeze on larger batches (somewhere 1k-10k).
+    
     b_train_y = binarize_labels(train_y[start_idx:end_idx])
     input_train_y = add_start_token(train_y[start_idx:end_idx])
     
@@ -258,11 +274,29 @@ def train_mini_epoch(model, start_idx, end_idx):
         print('> "{}"\n< "{}"'.format(input_string, output_string))
 
 
+
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------
+#MAIN_CODE
+
+texts, responses = read_csv_data('../data/twcs_10.csv')
+vocab, reverse_vocab, analyzer = build_vocab(texts, responses)
+train_x, test_x, train_y, test_y = build_vectors(texts, responses, analyzer)
+s2s_model = create_model()
+optimizer = Adam(lr=LEARNING_RATE, clipvalue=5.0)
+s2s_model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+
 from time import strftime, gmtime
 from datetime import timedelta
 
 
-training_time_limit = TRAINING_TIME_LIMIT  # seconds (notebooks terminate after 1 hour)
+
+training_time_limit = 1 * 60  # seconds (notebooks terminate after 1 hour)
 start_time = time.time()
 stop_after = start_time + training_time_limit
 
@@ -286,14 +320,13 @@ except TimesUpInterrupt:
 
 
 
-from keras.models import model_from_json
-
 model_json = s2s_model.to_json()
 with open("s2s_model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
 s2s_model.save_weights("s2s_model.h5")
 print("Saved model to disk")
+
 
 
 
